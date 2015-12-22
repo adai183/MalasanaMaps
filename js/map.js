846c518814d5ec628a61f215a21d95ba26afd157
adai183 <andreas.daiminger@gmail.com>
2015-12-22 18:25:32 +0100

    integrate weather animation

--- js/map.js
+++ js/map.js
@@ -56,8 +56,7 @@ var ViewModel = function() {
         title: name_string,
         map: map,
         draggable: false,
-        icon:   'http://www.google.com/mapfiles/arrow.png',
-        shadow: 'http://www.google.com/mapfiles/arrowshadow.png',      
+        icon:   'img/marker.svg', 
         animation: google.maps.Animation.DROP
       });
     }
@@ -109,7 +108,7 @@ var ViewModel = function() {
     
     try {
         // This next line makes `malasana` a new Google Map JavaScript Object
-        malasana = new google.maps.LatLng(40.424430, -3.701449);
+        malasana = new google.maps.LatLng(40.426394, -3.704878);
       } catch (err) {
         //if google map api didnt respond
         $('#map').hide();
@@ -118,12 +117,12 @@ var ViewModel = function() {
     
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
-      zoom: 15,
+      zoom: 17,
       center: malasana,
       mapTypeControl: true,
       mapTypeControlOptions: {
         style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-        position: google.maps.ControlPosition.TOP_RIGHT
+        position: google.maps.ControlPosition.LEFT_BOTTOM
       },
       zoomControl: true,
       zoomControlOptions: {
@@ -134,7 +133,9 @@ var ViewModel = function() {
       streetViewControlOptions: {
           position: google.maps.ControlPosition.RIGHT_BOTTOM
       },
-        styles: [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
+        styles: [{"featureType":"landscape.fill","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"on"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
+
+
       };
     if (typeof google != "undefined"){
       map = new google.maps.Map(mapCanvas, mapOptions);
